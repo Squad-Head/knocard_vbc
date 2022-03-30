224312bc239d32328ff1b0a581fec5940769edd8
dev-squadhead <squadhead247@gmail.com>
2022-03-30 17:13:59 +0600

    just update

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:knocard_ui/presentation/community/community_page.dart';
-import 'package:knocard_ui/presentation/home_page/home_page.dart';
 
 void main() {
   runApp(MyApp());
