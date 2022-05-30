169ca4ed45f8c3b74edf203994aaf65f1af25bcf
dev-squadhead <squadhead247@gmail.com>
2022-05-30 20:29:18 +0600

    home service page ui design complete

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:knocard_ui/presentation/home/home_page.dart';
+import 'package:knocard_ui/presentation/home_services/home_services.dart';
 
 import 'package:knocard_ui/style/color.dart';
 
@@ -12,7 +13,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
@@ -21,28 +21,17 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
-            // This is the theme of your application.
-            //
-            // Try running your application with "flutter run". You'll see the
-            // application has a blue toolbar. Then, without quitting the app, try
-            // changing the primarySwatch below to Colors.green and then invoke
-            // "hot reload" (press "r" in the console where you ran "flutter run",
-            // or simply save your changes to "hot reload" in a Flutter IDE).
-            // Notice that the counter didn't reset back to zero; the application
-            // is not restarted.
-            primarySwatch: Colors.blue,
-            primaryColor: KColor.primaryColor,
-            scaffoldBackgroundColor: KColor.backgroundColor),
+          primarySwatch: const MaterialColor(0xFF088ac6, KColor.mapColor),
+          scaffoldBackgroundColor: KColor.backgroundColor,
+        ),
         builder: (context, widget) {
-          //add this line
           ScreenUtil.setContext(context);
           return MediaQuery(
-            //Setting font does not change with system font size
             data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
             child: widget!,
           );
         },
-        home: HomePage(),
+        home: const HomeService(),
       ),
     );
   }
