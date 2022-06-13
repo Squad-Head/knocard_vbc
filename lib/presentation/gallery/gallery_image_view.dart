7407a0980f495d3379c47566d163ced6f7f4d436
dev-squadhead <squadhead247@gmail.com>
2022-06-13 16:07:41 +0600

    fixed

--- lib/presentation/gallery/gallery_image_view.dart
+++ lib/presentation/gallery/gallery_image_view.dart
@@ -5,24 +5,24 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 List<String> values = [
-  'assets/images/men_1.jpg',
-  'assets/images/grid1.png',
-  'assets/images/grid2.png',
-  'assets/images/grid3.png',
-  'assets/images/grid4.png',
-  'assets/images/grid5.png',
-  'assets/images/grid1.png',
-  'assets/images/grid3.png',
-  'assets/images/men_1.jpg',
-  'assets/images/men_1.jpg',
-  'assets/images/grid1.png',
-  'assets/images/grid2.png',
-  'assets/images/grid3.png',
-  'assets/images/grid4.png',
-  'assets/images/grid5.png',
-  'assets/images/grid1.png',
-  'assets/images/grid3.png',
-  'assets/images/men_1.jpg',
+  'https://images.unsplash.com/photo-1511367734837-f2956f0d8020?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1105&q=80',
+  'https://images.unsplash.com/photo-1453738773917-9c3eff1db985?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  'https://images.unsplash.com/photo-1655068867332-83374a86990c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80',
+  'https://images.unsplash.com/photo-1655062406430-f47dad378d0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  'https://images.unsplash.com/photo-1579118459333-b6c080d24b5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  'https://images.unsplash.com/photo-1521579880562-101f47676ee1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=745&q=80',
+  'https://images.unsplash.com/photo-1621370957995-9ef4b1379fa9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80',
+  'https://images.unsplash.com/photo-1502791451862-7bd8c1df43a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+  'https://images.unsplash.com/photo-1533070286885-87a85d8df9b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  'https://images.unsplash.com/photo-1588017104438-d559ef18ebbf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  'https://images.unsplash.com/photo-1476297820623-03984cf5cdbb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+  'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80',
+  'https://images.unsplash.com/photo-1510915228340-29c85a43dcfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  'https://images.unsplash.com/photo-1575089976121-8ed7b2a54265?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  'https://images.unsplash.com/photo-1543269664-76bc3997d9ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  'https://images.unsplash.com/photo-1543269664-76bc3997d9ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  'https://images.unsplash.com/photo-1542744095-fcf48d80b0fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80',
+  'https://images.unsplash.com/photo-1542744173-8e7e53415bb0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
 ];
 
 class GalleryImageViewPage extends HookConsumerWidget {
@@ -49,8 +49,7 @@ class GalleryImageViewPage extends HookConsumerWidget {
                       width: double.infinity,
                       color: Colors.grey,
                       child: Image(
-                        image: AssetImage(
-                          // 'assets/images/grid0.jpg',
+                        image: NetworkImage(
                           images[imageIndex.value],
                         ),
                       ),
@@ -71,8 +70,7 @@ class GalleryImageViewPage extends HookConsumerWidget {
                       width: double.infinity,
                       color: Colors.grey,
                       child: Image(
-                        image: AssetImage(
-                          // 'assets/images/grid0.jpg',
+                        image: NetworkImage(
                           images[imageIndex.value],
                         ),
                       ),
@@ -109,7 +107,7 @@ class GalleryImageViewPage extends HookConsumerWidget {
                           child: InkWell(
                             onTap: () {},
                             child: Image(
-                              image: AssetImage(
+                              image: NetworkImage(
                                 values[index],
                               ),
                             ),
