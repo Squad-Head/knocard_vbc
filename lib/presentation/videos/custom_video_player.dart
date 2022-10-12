bc5ee177f8a522724b5a5a256bbb7aee76cc341a
Kousik Ambani <rikousik@gmail.com>
2022-10-12 18:04:36 +0600

    check

--- lib/presentation/videos/custom_video_player.dart
+++ lib/presentation/videos/custom_video_player.dart
@@ -2,9 +2,6 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:knocard_ui/application/profile_provider.dart';
-import 'package:knocard_ui/infrastructure/reporting_repo.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 
 import 'network_video_player.dart';
 
